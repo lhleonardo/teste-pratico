f950e9335769bc49f9dcb1e89a5756376af3d40e
Leonardo Braz <lhleonardo05@gmail.com>
2022-02-19 00:49:53 -0300

    docs: adicionando o swagger no projeto

---
+++ src/main/java/br/com/wefin/testepratico/config/SpringFoxConfig.java
@@ -0,0 +1,40 @@
+package br.com.wefin.testepratico.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .contact(new Contact("Leonardo Braz", "http://github.com/lhleonardo", "lhleonardo05@gmail.com"))
+                .description("API desenvolvida para o Teste Prático da WeFin")
+                .title("API - PessoasManager")
+                .version("1.0.0")
+                .build();
+    }
+}
